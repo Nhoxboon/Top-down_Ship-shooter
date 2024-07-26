353b118ad8eeccb6e98fdc6639ddc0640b978e1a
Nhoxboon <trannhatanh156@gmail.com>
2024-07-26 10:38:28 +0700

    init project

---
+++ Assets/_Data/Script/InputManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager instance;
+    public static InputManager Instance { get => instance; }
+
+    [SerializeField] protected Vector3 mouseWorldPos;
+    public Vector3 MouseWorldPos { get => mouseWorldPos; }
+
+    private void Awake()
+    {
+        if (InputManager.instance != null)
+        {
+            Debug.LogError("ONLY 1 instances of InputManager exist");
+        }
+        InputManager.instance = this;
+    }
+
+    private void FixedUpdate()
+    {
+        GetMousePos();
+       
+    }
+
+    protected virtual void GetMousePos()
+    {
+        this.mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
